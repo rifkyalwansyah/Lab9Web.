93919d6086fb2f20608b8dc4921cc6269aacf62b
rifkyalwansyah <147989803+rifkyalwansyah@users.noreply.github.com>
2024-12-01 19:16:25 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Modularisasi PHP</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Modularisasi Menggunakan Require</h1>
+        </header>
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="kontak.php">Kontak</a>
+        </nav>
